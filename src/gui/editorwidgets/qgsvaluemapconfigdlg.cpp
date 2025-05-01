1340c724de050340fd1629e96eeda4e889b5632c
MoritzMoreira <hackenberg@dev-gis.com>
2025-05-01 09:40:13 +0200

    Allow null value checkbox
    Replacement of the button "Add NULL value" with a checkbox. The behaviour is now the same as of the value relation. fixes qgis#45088.

--- src/gui/editorwidgets/qgsvaluemapconfigdlg.cpp
+++ src/gui/editorwidgets/qgsvaluemapconfigdlg.cpp
@@ -17,7 +17,7 @@
 #include "moc_qgsvaluemapconfigdlg.cpp"
 
 #include "qgsattributetypeloaddialog.h"
-#include "qgsvaluemapfieldformatter.h"
+#include "qgsfieldformatter.h"
 #include "qgsapplication.h"
 #include "qgssettings.h"
 
@@ -42,8 +42,8 @@ QgsValueMapConfigDlg::QgsValueMapConfigDlg( QgsVectorLayer *vl, int fieldIdx, QW
   tableWidget->horizontalHeader()->setSectionsClickable( true );
   tableWidget->setSortingEnabled( true );
 
-  connect( addNullButton, &QAbstractButton::clicked, this, &QgsValueMapConfigDlg::addNullButtonPushed );
   connect( removeSelectedButton, &QAbstractButton::clicked, this, &QgsValueMapConfigDlg::removeSelectedButtonPushed );
+  connect( allowNullCheckBox, &QAbstractButton::toggled, this, &QgsEditorConfigWidget::changed );
   connect( loadFromLayerButton, &QAbstractButton::clicked, this, &QgsValueMapConfigDlg::loadFromLayerButtonPushed );
   connect( loadFromCSVButton, &QAbstractButton::clicked, this, &QgsValueMapConfigDlg::loadFromCSVButtonPushed );
   connect( tableWidget, &QTableWidget::cellChanged, this, &QgsValueMapConfigDlg::vCellChanged );
@@ -65,7 +65,7 @@ QVariantMap QgsValueMapConfigDlg::config()
 
     QString ks = ki->text();
     if ( ( ks == QgsApplication::nullRepresentation() ) && !( ki->flags() & Qt::ItemIsEditable ) )
-      ks = QgsValueMapFieldFormatter::NULL_VALUE;
+      ks = QgsFieldFormatter::NULL_VALUE;
 
     QVariantMap value;
 
@@ -82,6 +82,7 @@ QVariantMap QgsValueMapConfigDlg::config()
 
   QVariantMap cfg;
   cfg.insert( QStringLiteral( "map" ), valueList );
+  cfg.insert( QStringLiteral( "AllowNull" ), allowNullCheckBox->isChecked() );
   return cfg;
 }
 
@@ -117,6 +118,8 @@ void QgsValueMapConfigDlg::setConfig( const QVariantMap &config )
   }
 
   updateMap( orderedList, false );
+
+  allowNullCheckBox->setChecked( config.value( QStringLiteral( "AllowNull" ) ).toBool() );
 }
 
 void QgsValueMapConfigDlg::vCellChanged( int row, int column )
@@ -131,7 +134,7 @@ void QgsValueMapConfigDlg::vCellChanged( int row, int column )
   {
     // check cell value
     QTableWidgetItem *item = tableWidget->item( row, 0 );
-    if ( item && item->data( Qt::ItemDataRole::UserRole ) != QgsValueMapFieldFormatter::NULL_VALUE )
+    if ( item && item->data( Qt::ItemDataRole::UserRole ) != QgsFieldFormatter::NULL_VALUE )
     {
       const QString validValue = checkValueLength( item->text() );
       if ( validValue.length() != item->text().length() )
@@ -198,7 +201,7 @@ void QgsValueMapConfigDlg::updateMap( const QList<QPair<QString, QVariant>> &lis
 
   if ( insertNull )
   {
-    setRow( row, QgsValueMapFieldFormatter::NULL_VALUE, QStringLiteral( "<NULL>" ) );
+    setRow( row, QgsFieldFormatter::NULL_VALUE, QStringLiteral( "<NULL>" ) );
     ++row;
   }
 
@@ -246,7 +249,7 @@ void QgsValueMapConfigDlg::updateMap( const QList<QPair<QString, QVariant>> &lis
 
 QString QgsValueMapConfigDlg::checkValueLength( const QString &value )
 {
-  if ( value == QgsValueMapFieldFormatter::NULL_VALUE || value == QgsApplication::nullRepresentation() )
+  if ( value == QgsFieldFormatter::NULL_VALUE || value == QgsApplication::nullRepresentation() )
   {
     return value;
   }
@@ -272,7 +275,7 @@ void QgsValueMapConfigDlg::populateComboBox( QComboBox *comboBox, const QVariant
     {
       const QVariantMap valueMap = value.toMap();
 
-      if ( skipNull && valueMap.constBegin().value() == QgsValueMapFieldFormatter::NULL_VALUE )
+      if ( skipNull && valueMap.constBegin().value() == QgsFieldFormatter::NULL_VALUE )
         continue;
 
       comboBox->addItem( valueMap.constBegin().key(), valueMap.constBegin().value() );
@@ -283,7 +286,7 @@ void QgsValueMapConfigDlg::populateComboBox( QComboBox *comboBox, const QVariant
     const QVariantMap map = config.value( QStringLiteral( "map" ) ).toMap();
     for ( auto it = map.constBegin(); it != map.constEnd(); ++it )
     {
-      if ( skipNull && it.value() == QgsValueMapFieldFormatter::NULL_VALUE )
+      if ( skipNull && it.value() == QgsFieldFormatter::NULL_VALUE )
         continue;
 
       comboBox->addItem( it.key(), it.value() );
@@ -312,12 +315,12 @@ void QgsValueMapConfigDlg::setRow( int row, const QString &value, const QString 
   QTableWidgetItem *valueCell = nullptr;
   QTableWidgetItem *descriptionCell = new QTableWidgetItem( description );
   tableWidget->insertRow( row );
-  if ( value == QgsValueMapFieldFormatter::NULL_VALUE )
+  if ( value == QgsFieldFormatter::NULL_VALUE )
   {
     QFont cellFont;
     cellFont.setItalic( true );
     valueCell = new QTableWidgetItem( QgsApplication::nullRepresentation() );
-    valueCell->setData( Qt::ItemDataRole::UserRole, QgsValueMapFieldFormatter::NULL_VALUE );
+    valueCell->setData( Qt::ItemDataRole::UserRole, QgsFieldFormatter::NULL_VALUE );
     valueCell->setFont( cellFont );
     valueCell->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
     descriptionCell->setFont( cellFont );
@@ -355,11 +358,6 @@ void QgsValueMapConfigDlg::copySelectionToClipboard()
   }
   mimeData->setData( QStringLiteral( "text/plain" ), clipboardText.toUtf8() );
   QApplication::clipboard()->setMimeData( mimeData.release() );
-}
-
-void QgsValueMapConfigDlg::addNullButtonPushed()
-{
-  setRow( tableWidget->rowCount() - 1, QgsValueMapFieldFormatter::NULL_VALUE, QStringLiteral( "<NULL>" ) );
 }
 
 void QgsValueMapConfigDlg::loadFromLayerButtonPushed()
@@ -413,7 +411,7 @@ void QgsValueMapConfigDlg::loadMapFromCSV( const QString &filePath )
     QString key = ceils[0];
     QString val = ceils[1];
     if ( key == QgsApplication::nullRepresentation() )
-      key = QgsValueMapFieldFormatter::NULL_VALUE;
+      key = QgsFieldFormatter::NULL_VALUE;
     map.append( qMakePair( key, val ) );
   }
 
